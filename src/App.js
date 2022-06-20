ae79021ccb5a6d85ff4e224a5579e3d67ba50882
Tanya Musara <43385626+tanyamusara@users.noreply.github.com>
2022-06-20 13:31:27 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
+import AboutPage from './pages/AboutPage';
+import { FeedbackProvider } from './context/FeedbackContext';
+import AboutIconLink from './components/AboutIconLink';
 
+//main function
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    return (
+        <FeedbackProvider>
+        <Router>
+        <Header />
+        <div className="container">
+            <Routes>
+            <Route exact path='/' element={
+            <>
+            <FeedbackForm />
+            <FeedbackStats />
+            <FeedbackList />
+            </>
+            }> 
+            </Route>
+            <Route path='/about' element={<AboutPage />}/>   
+            </Routes>
+
+            <AboutIconLink/>
+        </div>
+    </Router>
+    </FeedbackProvider>
+    )
 }
+
+
+// ##############################################################################
+//redudant code
+/**
+ * const title = 'Blog Post';
+const body = 'This is my blog post';
+const comments = [
+    {id:1, text: 'Comment one'},
+    {id:2, text: 'Comment two'},
+    {id:3, text: 'Comment three'},
+];
+
+const loading = false;
+
+const showComments = true;
+
+const commentBlock = (  <div className="comments">
+<h3>Comments ({comments.length})</h3>
+<ul>
+   {comments.map((comment, index) => (
+      <li key={index}>{comment.text}</li>
+   ))} 
+</ul>
+</div>);
+
+if (loading) return <h1>Loading...</h1> 
+ */
+// ##############################################################################
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+/**
+ * variable container
+ * gets root element from the dom
+ */
+const container = document.getElementById('root');
+
+/**
+ * variable root
+ * gets creats root using the container variable
+ */
+const root = ReactDOM.createRoot(container);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ <React.StrictMode>
+<App/>
+</React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
